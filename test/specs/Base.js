98ab6d846a30636d36f395adf517eb84eac1dccd
Don <don@thegriffins.org>
2017-09-12 01:02:25 -0500

    wip

---
+++ test/specs/Base.js
@@ -0,0 +1,8 @@
+describe('Base', function () {
+    describe('life-cycle', function () {
+        it('should be able to create an instance', function () {
+            let x = 0;
+            console.log(x);
+        });
+    });
+});
